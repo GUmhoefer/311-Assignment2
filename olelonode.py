9158415f6adf5e5ea53679bcda4761049269f3e4
GUmhoefer <gregoru@hawaii.edu>
2024-09-17 19:52:35 -1000

    Update node class
    Include parent pointer variable. Code to split phrases into lists of words, and also to join them with spaces in the str method.

--- olelonode.py
+++ olelonode.py
@@ -1,14 +1,15 @@
 class OleloNode:
     def __init__(self, phrase_olelo, phrase_english, exp_olelo, exp_english):
-        self.phrase_olelo = phrase_olelo
-        self.phrase_english = phrase_english
+        self.phrase_olelo = phrase_olelo.split()
+        self.phrase_english = phrase_english.split()
         self.exp_olelo = exp_olelo
         self.exp_english = exp_english
 
         self.left = None  # Left child node
         self.right = None  # Right child node
+        self.parent = None # Pointer to parent node
 
         self.tree_height = 1  # Stores the height this node's tree
 
     def __str__(self):
-        return f"Phrase in Olelo Hawaiʻi:\n'{self.phrase_olelo}, '\n','Phrase in English:\n'{self.phrase_english}, '\nExplanation in Olelo Hawaiʻi:\n',{self.exp_olelo}, '\nExplanation in English:\n', {self.exp_english}"
+        return f"Phrase in Olelo Hawaiʻi:\n{' '.join(self.phrase_olelo)}\nPhrase in English:\n{' '.join(self.phrase_english)}\nExplanation in Olelo Hawaiʻi:\n{self.exp_olelo}\nExplanation in English:\n{self.exp_english}"
