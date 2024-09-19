ecafaf643068f0411b405d997c2557917522675d
GUmhoefer <156273764+GUmhoefer@users.noreply.github.com>
2024-09-19 00:01:09 -1000

    Updated Successor and Predecessor
    Adjusted successor and predecessor methods to match the structure of the node and tree. Also corrected edge case where the grandparent node was NIL node above the root, causing a crash.

--- test.py
+++ test.py
@@ -12,5 +12,7 @@ class main():
     tree.insert("Hoʻokahi nō lā o ka malihini.", "A stranger only for a day.", "After the first day as a guest, one must help with the work.", "After the first day as a guest, one must help with the work.")
     tree.insert("Baialua, ʻāina kū pālua i ka laʻi.", "Baialua, land that stands doubly becalmed.", "Said in admiration for Waialua, Oʻahu, where the weather wa usually pleasant and the life of the people tranquil.", "Said in admiration for Waialua, Oʻahu, where the weather wa usually pleasant and the life of the people tranquil.")
 
-    tree.in_order()
+    print(tree.successor("Waialua, ʻāina kū pālua i ka laʻi."))
+    print(tree.predecessor("Waialua, ʻāina kū pālua i ka laʻi."))
+    print(tree.successor("Bleep bloop"))
 main()
